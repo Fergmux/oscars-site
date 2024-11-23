04dd72e43c6023b10a6e9affaefc420d9e0e1d08
Fergus Mullineux <fergmux@gmail.com>
2024-11-23 16:57:02 +0000

    fix: error

--- src/components/Toast.vue
+++ src/components/Toast.vue
@@ -1,32 +1,42 @@
 <template>
   <div
     id="toast"
-    class="fixed left-0 right-0 flex flex-col justify-center margin-auto hide"
+    class="margin-auto hide fixed left-0 right-0 flex flex-col justify-center"
   >
     <div
       id="static-example"
-      class="block max-w-full px-5 mx-auto mb-3 text-sm rounded-lg shadow-lg pointer-events-auto bg-mint-500 bg-clip-padding"
+      class="bg-mint-500 pointer-events-auto mx-auto mb-3 block max-w-full rounded-lg bg-clip-padding px-5 text-sm shadow-lg"
       role="alert"
       aria-live="assertive"
       aria-atomic="true"
       data-mdb-autohide="false"
     >
-      <div class="p-3 text-white break-words rounded-lg bg-mint-500">
+      <div class="bg-mint-500 break-words rounded-lg p-3 text-white">
         {{ text }}
       </div>
     </div>
   </div>
 </template>
 
+<script setup>
+import { ref } from 'vue';
+
+const text = ref('');
+</script>
+
 <style>
 .hide {
   opacity: 0;
   top: 50px;
-  transition: opacity 0.5s ease-in-out, top 0.5s ease-in-out;
+  transition:
+    opacity 0.5s ease-in-out,
+    top 0.5s ease-in-out;
 }
 .show {
   opacity: 1;
   top: 40px;
-  transition: opacity 0.5s ease-in-out, top 0.5s ease-in-out;
+  transition:
+    opacity 0.5s ease-in-out,
+    top 0.5s ease-in-out;
 }
 </style>
